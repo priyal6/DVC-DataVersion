436cfb1a6ce3b2ec1d04b98d999f2ba4c0147a2c
priyal6 <layirps06@gmail.com>
2025-10-07 14:03:35 +0100

    second version of the data saved

--- code_1.py
+++ code_1.py
@@ -9,6 +9,11 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
 
 df = pd.DataFrame(data)
 
+#add new to df for V2
+new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+df.loc[len(df.index)] = new_row_loc
+
+
 data_dir = 'data'
 os.makedirs(data_dir,exist_ok=True)
 
